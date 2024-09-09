9dbf7098cb73b3f78a98a179cf62205de5ce9115
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-09-09 22:00:39 +0600

    add date-time-picker with modal

---
+++ src/components/form/AppDateTimePicker.tsx
@@ -0,0 +1,83 @@
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { cn } from '@/lib/utils';
+import { StaticDateTimePicker } from '@mui/x-date-pickers';
+import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
+import { CalendarIcon } from '@radix-ui/react-icons';
+import { format } from 'date-fns';
+import moment from 'moment';
+import { useRef } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { Button } from '../ui/button';
+
+interface AppDateTimePickerProps {
+  name: string;
+  label?: string;
+  placeholder?: string;
+}
+
+export function AppDateTimePicker({
+  name,
+  label,
+  placeholder,
+}: AppDateTimePickerProps) {
+  const form = useFormContext();
+
+  const triggerRef = useRef<HTMLButtonElement>(null);
+
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          {label && <FormLabel>{label}</FormLabel>}
+          <Dialog>
+            <DialogTrigger asChild>
+              <FormControl>
+                <Button
+                  variant={'outline'}
+                  className={cn(
+                    'w-full pl-3 text-left font-normal',
+                    !field.value && 'text-muted-foreground'
+                  )}
+                  ref={triggerRef}
+                >
+                  {field.value ? (
+                    format(field.value, 'PPpp')
+                  ) : (
+                    <span>{placeholder || 'Pick a date'}</span>
+                  )}
+                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                </Button>
+              </FormControl>
+            </DialogTrigger>
+            <DialogContent className="max-w-sm" overlay={false}>
+              <StaticDateTimePicker
+                value={moment(field.value)}
+                onChange={(value) => field.onChange(value?.toDate())}
+                onClose={() =>
+                  (triggerRef.current as HTMLButtonElement).click()
+                }
+                viewRenderers={{
+                  hours: renderTimeViewClock,
+                  minutes: renderTimeViewClock,
+                  seconds: renderTimeViewClock,
+                }}
+                ampmInClock={true}
+              />
+            </DialogContent>
+          </Dialog>
+
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
