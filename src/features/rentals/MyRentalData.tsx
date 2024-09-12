f80eba887951ea502ccba8969970fabf1aa43d3f
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-09-13 00:50:01 +0600

    clean my rental data table

--- src/features/rentals/MyRentalData.tsx
+++ src/features/rentals/MyRentalData.tsx
@@ -2,15 +2,9 @@ import { IRental } from '@/constants';
 import { DataTable } from '@/features/table';
 import {
   ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
   getCoreRowModel,
-  getFilteredRowModel,
-  getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table';
-import { useState } from 'react';
 
 interface MyRentalProps {
   data: IRental[];
@@ -23,27 +17,10 @@ export default function MyRentalData({
   isLoading,
   columns,
 }: MyRentalProps) {
-  const [sorting, setSorting] = useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
-  const [rowSelection, setRowSelection] = useState({});
-
   const table = useReactTable({
     data,
     columns: columns,
     getCoreRowModel: getCoreRowModel(),
-    onSortingChange: setSorting,
-    getSortedRowModel: getSortedRowModel(),
-    onColumnFiltersChange: setColumnFilters,
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-      rowSelection,
-    },
   });
 
   return (
