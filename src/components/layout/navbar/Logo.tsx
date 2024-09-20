5c9e0cb7512182911368c2650695ccde3180bae2
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-09-20 18:14:26 +0600

    update navbar logo

--- src/components/layout/navbar/Logo.tsx
+++ src/components/layout/navbar/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link to="/">
       <h1 className="text-4xl font-bold font-bebas">
-        <span className="text-primary">BIKE</span>LAGBE
+        <span className="text-primary">RIDE</span>X
       </h1>
     </Link>
   );
