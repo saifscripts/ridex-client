29d3593ae5e5de2c80cd9895f92a6067f42b659a
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-12-02 22:44:38 +0600

    update navbar container area

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -33,37 +33,39 @@ const Navbar = () => {
   return (
     <div
       className={cn(
-        'fixed w-full h-[64px] border-b flex justify-between items-center px-6 transition-top duration-300 z-50 bg-background',
+        'fixed w-full h-[64px] border-b transition-top duration-300 z-50 bg-background',
         {
           'top-0': visible,
           'top-[-64px]': !visible,
         }
       )}
     >
-      {/* Navbar Content: Left */}
-      <Logo />
+      <div className="container h-full flex justify-between items-center">
+        {/* Navbar Content: Left */}
+        <Logo />
 
-      {/* Navbar Content: Center (Desktop) */}
-      <div className="hidden md:flex gap-8 justify-center items-center basis-1/3">
-        {navLinks.map((item) => (
-          <NavItem key={item.path} to={item.path}>
-            {item.text}
-          </NavItem>
-        ))}
-      </div>
+        {/* Navbar Content: Center (Desktop) */}
+        <div className="hidden md:flex gap-8 justify-center items-center basis-1/3">
+          {navLinks.map((item) => (
+            <NavItem key={item.path} to={item.path}>
+              {item.text}
+            </NavItem>
+          ))}
+        </div>
 
-      {/* Navbar Content: Right (Desktop) */}
-      <div className="hidden md:flex gap-2 justify-end items-center basis-1/3">
-        <ModeToggle />
-        <Separator orientation="vertical" className="h-8" />
-        {user ? <UserDropdown /> : <AuthButtons />}
-      </div>
+        {/* Navbar Content: Right (Desktop) */}
+        <div className="hidden md:flex gap-2 justify-end items-center basis-1/3">
+          <ModeToggle />
+          <Separator orientation="vertical" className="h-8" />
+          {user ? <UserDropdown /> : <AuthButtons />}
+        </div>
 
-      {/* Navbar Content: Right (Mobile) */}
-      <div className="flex gap-2 items-center justify-end md:hidden basis-1/3">
-        <ModeToggle />
-        {user && <UserDropdown />}
-        <Sidebar />
+        {/* Navbar Content: Right (Mobile) */}
+        <div className="flex gap-2 items-center justify-end md:hidden basis-1/3">
+          <ModeToggle />
+          {user && <UserDropdown />}
+          <Sidebar />
+        </div>
       </div>
     </div>
   );
