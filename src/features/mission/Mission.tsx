ba4e54589287657b16b377b5feed6f8532f27865
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-12-13 16:52:05 +0600

    [about]: update mission section width and content

--- src/features/mission/Mission.tsx
+++ src/features/mission/Mission.tsx
@@ -1,22 +1,69 @@
+import Container from '@/components/layout/Container';
 import {
   Section,
   SectionDescription,
   SectionTitle,
 } from '@/components/layout/Section';
+import { CheckCircleIcon } from 'lucide-react';
 
 export default function Mission() {
   return (
     <Section className="container">
       <SectionTitle>Our Mission</SectionTitle>
-      <SectionDescription className="mb-0">
-        At RideX, our mission is to transform urban mobility through innovative
-        bike-sharing solutions. We envision a future where cities are more
-        livable, sustainable, and connected. Our purpose is to provide
-        accessible, eco-friendly transportation options that reduce carbon
-        emissions and traffic congestion. We are committed to empowering
-        communities, promoting health and wellness, and contributing to a
-        greener planet for generations to come.
+      <SectionDescription>
+        Transforming Cities, One Ride at a Time.
       </SectionDescription>
+      <Container>
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-8">
+          <img
+            src="https://res.cloudinary.com/dz6h5okzp/image/upload/v1734086944/harley-davidson-6_OBa-II5Go-unsplash_hklegd.jpg"
+            alt="Mission Image"
+            className="w-full lg:w-1/2 rounded-lg"
+          />
+          <div>
+            <h2 className="text-2xl font-bold mb-4 text-center lg:text-left">
+              Transforming Urban Mobility
+            </h2>
+            <p className="mb-2 text-center lg:text-left">
+              RideX aims to transform urban mobility with innovative
+              bike-sharing solutions, envisioning cities that are livable,
+              sustainable, and connected.
+            </p>
+            <ul className="list-disc pl-6 lg:pl-2 space-y-2">
+              <li className="flex gap-2">
+                <CheckCircleIcon size={16} className="text-success" />
+                Encourage sustainable mobility solutions to reduce urban carbon
+                footprints.
+              </li>
+              <li className="flex gap-2">
+                <CheckCircleIcon size={16} className="text-success" />
+                Provide a seamless bike-sharing platform that connects people to
+                their destinations.
+              </li>
+              <li className="flex gap-2">
+                <CheckCircleIcon size={16} className="text-success" />
+                Encourage an active lifestyle by promoting cycling as a
+                transportation option.
+              </li>
+              <li className="flex gap-2">
+                <CheckCircleIcon size={16} className="text-success" />
+                Utilize cutting-edge technology for user-friendly booking, and
+                payment experiences.
+              </li>
+              <li className="flex gap-2">
+                <CheckCircleIcon size={16} className="text-success" />
+                Collaborate with cities to integrate bike-sharing into public
+                transportation systems.
+              </li>
+              <li className="flex gap-2">
+                <CheckCircleIcon size={16} className="text-success" />
+                Advocate for urban planning that prioritizes non-motorized
+                transport solutions.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Container>
     </Section>
   );
 }
