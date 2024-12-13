336669f03073178dca543a283e53c91e740190c1
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-12-13 15:45:39 +0600

    [bike]: fix bike card skeleton

--- src/features/bike/BikesSkeleton.tsx
+++ src/features/bike/BikesSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function BikesSkeleton() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 my-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
       {[...Array(12)].map((_, index) => (
         <CardSkeleton key={index} />
       ))}
